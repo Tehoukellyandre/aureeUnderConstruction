4ecd23ad3c4fa3841bf5c11f1e62cc9748b446ed
tehoukellyandre <kellytehou2@gmail.com>
2025-12-11 10:41:14 +0100

    Referencement ceo

--- src/page.tsx
+++ src/page.tsx
@@ -182,10 +182,10 @@ export default function UnderConstruction() {
                           <p className="text-xs text-muted-foreground mt-1">03 BP 23 cidex 03</p>
                         </div>
                       </div>
-                      <div className="w-full h-48 rounded-lg overflow-hidden border border-border">
+                      <div className="w-fit h-48 rounded-lg overflow-hidden border border-border">
                         <iframe 
                           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.3289991631964!2d-3.9191236264188736!3d5.366687635527241!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfc1eda7f8d05967%3A0x115312368c8cfab0!2sAUREE%20SERVICES!5e0!3m2!1sfr!2sbj!4v1765408419838!5m2!1sfr!2sbj" 
-                          width="600" height="300" 
+                          width="430" height="300" 
                           style ={{ border: 0 }}
                           allowFullScreen 
                           loading="lazy" 
